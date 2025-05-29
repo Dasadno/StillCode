3b5aaba2392fdc61a46301fe2f0536d78215c642
Watermelon <148169199+Dasadno@users.noreply.github.com>
2025-05-29 13:53:33 +0500

    Delete App directory

--- App/model/users.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	Name        string `json:"name"`
-	Email       string `json:"email"`
-	Id          int    `json:"id"`
-	Rating      int    `json:"rating"`
-	TasksSolved int    `json:"tasksCount"`
-}
