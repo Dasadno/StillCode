d9e0642b21a8568440d902b8aa9c2978f003e74a
dasadno <yakaleks96@gmail.com>
2025-09-20 19:31:27 +0500

    small fixes

--- client/src/styles/base/TailWindcfg.js
+++ client/src/styles/base/TailWindcfg.js
@@ -1,10 +1,25 @@
  tailwind.config = {
             theme: {
                 extend: {
+                    keyfreames: {
+                        gradient: {
+                            '0%, 100%': { 'background-position': '0% 50%' },
+                            '50%': { 'background-position': '100% 50%' },
+                            '100%': { 'background-position': '0% 50%' },
+                        },
+                    },
+                    animation: {
+                        gradient: 'gradient 3s ease infinite',
+                    },
                     colors: {
                         'primary': '#ff3333',
                         'primary-dark': '#cc0000',
-                        'dark': '#121212',
+                        'primary-darker': '#221f1fff',
+
+                        'secondary-dark': '#333030ff',
+                        'secondary-darker': '#2b2727ff',
+
+                        'dark': '#131212ff',
                         'darker': '#0a0a0a',
                         'light': '#f8f8f8',
                     },
